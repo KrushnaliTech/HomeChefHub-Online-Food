c525ffd2d339308c81f6c5b5e4c68285120a3787
KrushnaliTech <jagtapkrushnali11@gmail.com>
2026-07-26 23:57:10 +0530

    Initial commit

---
+++ JS/register.js
@@ -0,0 +1,8 @@
+const registerButton=document.getElementById('signupBtn');
+const registerForm=document.getElementById('SignUp');
+
+registerButton.addEventListener('click',function(){
+    registerForm.style.display="none";
+    
+})
+
